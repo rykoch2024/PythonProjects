22521b86e90b8ab7dfdeb80f9545cfb42948d441
Koch <rykoch2024@gmail.com>
2025-02-14 23:31:08 -0500

    basic madlibs project

--- madLibs/madLibs.py
+++ madLibs/madLibs.py
@@ -5,3 +5,27 @@ Madlibs project Goals
 2. Multiple inserts for verbs, adjatives, nouns
 3. Random sentence selection
 """
+
+def getVerb():
+    return input("Enter Verb: ")
+
+def getAdj():
+    return input("Enter Adjective: ")
+
+def getNoun():
+    return input("Enter Noun: ")
+
+def getColor():
+    return input("Enter Color: ")
+
+
+noun1 = getNoun()
+verb = getVerb()
+adj1 = getAdj()
+noun2 = getNoun()
+adj2 = getAdj()
+
+"""The little red rabbit ran over the river"""
+
+print("The " + adj1 + " " + adj2 + " " + noun1 + " " + verb + " over the " + noun2 + "." )
+
