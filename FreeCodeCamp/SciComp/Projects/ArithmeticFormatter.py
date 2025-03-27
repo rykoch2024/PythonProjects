8c47fc14865bdc08285f3ac64f574153c3eb4820
Koch <rykoch2024@gmail.com>
2025-03-27 17:22:12 -0400

    delete files from old directories & build Arithmetic Formatter Project

---
+++ FreeCodeCamp/SciComp/Projects/ArithmeticFormatter.py
@@ -0,0 +1,33 @@
+# Tasks
+# [] Split item into 3 parts: int1, int2, and symbol
+# [] Determine length of longer number
+# [] Determine if + or -
+# []
+# []
+# []
+# []
+# []
+# []
+# []
+
+
+
+
+
+
+
+
+
+
+
+
+def arithmetic_arranger(problems, show_answers=False):
+    firstNum = []
+    sign = []
+    secondNum = []
+    print(len(problems))
+
+    return problems
+
+
+print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
