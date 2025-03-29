b503775eb910a0734bd2f936d2e38281bc470ad3
Koch <rykoch2024@gmail.com>
2025-03-28 22:49:49 -0400

    Remove whitespace error from show_answer true option. Complete Challenge

--- FreeCodeCamp/SciComp/Projects/ArithmeticFormatter.py
+++ FreeCodeCamp/SciComp/Projects/ArithmeticFormatter.py
@@ -12,7 +12,7 @@ def arithmetic_arranger(problems, show_answers=False):
     result = []
     lineDraw = []
     if len(problems) > 5:
-        raise ValueError('Error: Too many problems.')
+        return "Error: Too many problems."
     for equation in problems:
         parts = equation.split()
         line = ''
@@ -20,11 +20,11 @@ def arithmetic_arranger(problems, show_answers=False):
 
 #Error tests: 1 - Numbers only contain digits, 2: Numbers 4 digits or less, 3: operator is + or -
         if not (parts[1] == '+' or parts[1] == '-'):
-            raise ValueError("Error: Operator must be '+' or '-'.")
+            return "Error: Operator must be '+' or '-'."
         elif max(len(parts[0]), len(parts[2])) > 4:
-            raise ValueError('Error: Numbers cannot be more than four digits.')
+            return "Error: Numbers cannot be more than four digits."
         elif not (parts[0].isdigit() and parts[2].isdigit()):
-            raise ValueError('Error: Numbers must only contain digits.')
+            return "Error: Numbers must only contain digits."
 
     #Expression Evaluation        
         if parts[1] == '+':
@@ -57,7 +57,7 @@ def arithmetic_arranger(problems, show_answers=False):
     problems += ''.join(lineDraw).removesuffix('    ')
 
     if show_answers:
-        problems += '\n'+''.join(result)
+        problems += '\n'+''.join(result).removesuffix('    ')
     return problems
 
-print(f'\n{arithmetic_arranger(["3801 - 2", "123 + 49"],)}')
+print(f'\n{arithmetic_arranger(["3 + 855", "988 + 40"], True)}')
