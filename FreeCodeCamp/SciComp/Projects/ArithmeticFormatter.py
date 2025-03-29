9e1233fc5bdb4d9a78fda4865e575b9f6e52895d
Koch <rykoch2024@gmail.com>
2025-03-28 22:06:45 -0400

    format single answer display for show_answers True and False

--- FreeCodeCamp/SciComp/Projects/ArithmeticFormatter.py
+++ FreeCodeCamp/SciComp/Projects/ArithmeticFormatter.py
@@ -2,25 +2,28 @@
 # [] Split item into 3 parts: int1, int2, and symbol
 # [] Determine length of longer number
 # [] Determine if + or -
-# []
-# []
-# []
-# []
-# []
-# []
-# []
 
 
+
+def sizeMatch(number, length):
+    sizeDifference = length - len(number)
+    result = ''
+    for _ in range(sizeDifference):
+        result += ' '
+    result += number    
+    return result
 
 def arithmetic_arranger(problems, show_answers=False):
     firstNum = []
-    sign = []
     secondNum = []
     result = []
+    lineDraw = []
     if len(problems) > 5:
         raise ValueError('Error: Too many problems.')
     for equation in problems:
         parts = equation.split()
+        line = ''
+        answer = ''
 #Error tests: 1 - Numbers only contain digits, 2: Numbers 4 digits or less, 3: operator is + or -
         if not (parts[1] == '+' or parts[1] == '-'):
             raise ValueError("Error: Operator must be '+' or '-'.")
@@ -28,27 +31,45 @@ def arithmetic_arranger(problems, show_answers=False):
             raise ValueError('Error: Numbers cannot be more than four digits.')
         elif not (parts[0].isdigit() and parts[2].isdigit()):
             raise ValueError('Error: Numbers must only contain digits.')
-        else:
-            firstNum.append(parts[0])
-            sign.append(parts[1])
-            secondNum.append(parts[2])
-        
-        if parts[1] == '+':
-            result.append(int(parts[0]) + int(parts[2]))
-        if parts[1] == '-':
-            equation.append(int(parts[0]) - int(parts[2]))
 
+    #Expression Evaluation        
+        if parts[1] == '+':
+            answer = int(parts[0]) + int(parts[2])
+        if parts[1] == '-':
+            answer = int(parts[0]) - int(parts[2])
+
+    # initial size Adjustments
+        longer = max(len(parts[0]), len(parts[2]))
+        parts[0] = '  ' + sizeMatch(parts[0], longer) + '    '
+        parts[2] = parts[1]+ ' ' + sizeMatch(parts[2], longer) + '    '
+
+        
+        
+
+        if show_answers:
+            answer = str(answer)
+            newLonger = max(len(parts[0]) - 4,len(parts[0]) - 4, len(answer))
+            parts[0]  = sizeMatch(parts[0], newLonger)
+            parts[2]  = sizeMatch(parts[2], newLonger)
+            result.append(sizeMatch(answer, newLonger) + '    ')
+        
+        for _ in range(len(parts[2]) - 4):
+            line += '-'
+
+        firstNum.append(parts[0])
+        secondNum.append(parts[2])
+        lineDraw.append(line + '    ')
         
             
         
 
 
     print(firstNum)
-    print(sign)
     print(secondNum)
-    print(equation)
+    print(lineDraw)
+    print(result)
     return problems
 
 
-print(f'\n{arithmetic_arranger(["32 + 699"])}')
+print(f'\n{arithmetic_arranger(["9932 + 9"], True)}')
 # print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
