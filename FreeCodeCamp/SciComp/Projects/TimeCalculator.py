bfebb77ed44a9a49603b7d0e761d9edb974b4655
Koch <rykoch2024@gmail.com>
2025-04-01 14:56:47 -0400

    Handle next day or multiple days

--- FreeCodeCamp/SciComp/Projects/TimeCalculator.py
+++ FreeCodeCamp/SciComp/Projects/TimeCalculator.py
@@ -1,5 +1,7 @@
 # [] Break start into hour, minute and day part
 # [] Break duration into hour, minute
+# [] handle multiple days
+
 
 
 def add_time(start, duration):
@@ -7,6 +9,7 @@ def add_time(start, duration):
     new_time = ''
     final_day_part = ''
     day_change = ''
+    total_days = 0
 
     add_duration = []
     combine_Time = []
@@ -21,21 +24,32 @@ def add_time(start, duration):
     if day_part == 'PM':
         start_time[0] = int(start_time[0]) + 12
     combine_Time = [int(start_time[0]) + int(add_duration[0]), int(start_time[1]) + int(add_duration[1])]
-    if combine_Time[1] >= 60:
+# Minute Handling
+    while combine_Time[1] >= 60:
         combine_Time[1] -= 60
         combine_Time[0] += 1
-    if combine_Time[0] >= 12 and combine_Time[0] < 24:
+
+# Day Handling
+    while combine_Time[0] >= 24:
+        if combine_Time[0] >= 24:
+            total_days += 1
+            combine_Time[0] -= 24
+
+# Hour Handling
+    if combine_Time[0] >= 12:
         final_day_part = 'PM'
         if combine_Time[0] > 12:
             combine_Time[0] -= 12
-    else: 
+
+    else:
         final_day_part = 'AM'
-        if combine_Time[0] >= 24:
-            day_change = ' (next day)'
-            if combine_Time[0] == 24:
-                combine_Time[0] -= 12
-            elif combine_Time[0] > 24:
-                combine_Time[0] -= 24
+        if combine_Time[0] == 0:
+            combine_Time[0] += 12
+
+    if total_days == 1:
+        day_change = ' (next day)'
+    elif total_days > 1:
+        day_change = ' (' + str(total_days) + ' days later)'
 
     new_time = f'{combine_Time[0]}:{combine_Time[1]:02d} {final_day_part}{day_change}'
     return new_time
@@ -43,3 +57,4 @@ def add_time(start, duration):
 print(add_time('3:30 PM', '2:12'))
 print(add_time('11:55 AM', '3:12'))
 print(add_time('2:59 AM', '24:00'))
+print(add_time('11:59 PM', '24:05'))
