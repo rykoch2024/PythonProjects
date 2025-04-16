4de0a9c8043bc105973ba97f52d06601c25054f2
Ryan <rykoch2024@gmail.com>
2025-04-16 17:05:22 -0400

    Build check_funds and get_balance methods

--- FreeCodeCamp/SciComp/Projects/BudgetApp.py
+++ FreeCodeCamp/SciComp/Projects/BudgetApp.py
@@ -8,6 +8,30 @@ class Category:
         self.ledger.append({'amount': amount, 'description': description})
         self._budget += amount
 
+    def withdraw(self, amount, description=''):
+        if self.check_funds(amount):
+            self.ledger.append({'amount': -1 * amount, 'description': description})
+            self._budget -= amount
+            return True
+        return False
+    
+    def get_balance(self):
+        return self._budget
+
+    def transfer():
+        pass
+
+    #Required for use by withdraw and transfer
+    def check_funds(self, amount):
+        if amount > self._budget:
+            return False
+        return True
 
 def create_spend_chart(categories):
     pass
+
+
+food = Category('food')
+food.deposit(900, 'deposit')
+food.withdraw(45.67, 'milk, cereal, eggs, bacon, bread')
+print('test')
