949bc3cb751c9014ede59b0b170737d093d59bd6
Ryan <rykoch2024@gmail.com>
2025-04-25 13:42:38 -0400

    Build transfer and begin _str_

--- FreeCodeCamp/SciComp/Projects/BudgetApp.py
+++ FreeCodeCamp/SciComp/Projects/BudgetApp.py
@@ -3,6 +3,36 @@ class Category:
         self.catName = catName
         self.ledger = []
         self._budget = 0.0
+
+    def __str__(self):
+        printOut = ''
+        # Title Line(FUNCTIONAL)
+        numAstrisk = (30 - len(self.catName)) // 2
+
+        for _ in range(numAstrisk):
+            printOut += '*'
+        printOut += self.catName
+
+        while len(printOut) < 30:
+            printOut += '*'
+
+        printOut += '\n'
+        
+
+        # There has to be a better way than this...
+        #Item List
+        for item in self.ledger:
+            line = ''
+            line += f'{item['description']:<23}'
+            line += '\n'
+
+            printOut += line
+
+
+
+        # Total(FUNCTIONAL)
+        printOut += f'Total: {self._budget}'
+        return printOut
     
     def deposit(self, amount, description=''):
         self.ledger.append({'amount': amount, 'description': description})
@@ -18,8 +48,12 @@ class Category:
     def get_balance(self):
         return self._budget
 
-    def transfer():
-        pass
+    def transfer(self, amount, other):
+        if self.check_funds(amount):
+            self.withdraw(amount, f'Transfer to {other.catName}')
+            other.deposit(amount, f'Transfer from {self.catName}')
+            return True
+        return False
 
     #Required for use by withdraw and transfer
     def check_funds(self, amount):
@@ -34,4 +68,4 @@ def create_spend_chart(categories):
 food = Category('food')
 food.deposit(900, 'deposit')
 food.withdraw(45.67, 'milk, cereal, eggs, bacon, bread')
-print('test')
+print(food)
