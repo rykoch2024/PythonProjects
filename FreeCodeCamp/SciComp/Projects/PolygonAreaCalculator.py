224ed9ff564f00e37e44175bb0d53a5922d61a43
Ryan <rykoch2024@gmail.com>
2025-05-18 21:14:49 -0400

    Begin Poly Area Calc Project from FreeCodeCamp

---
+++ FreeCodeCamp/SciComp/Projects/PolygonAreaCalculator.py
@@ -0,0 +1,24 @@
+class Rectangle:
+    def set_width():
+        pass
+
+    def set_height():
+        pass
+
+    def get_area():
+        pass
+
+    def get_perimeter():
+        pass
+
+    def get_diagonal():
+        pass
+
+    def get_picture():
+        pass
+
+    def get_amount_inside():
+        pass
+
+class Square(Rectangle):
+    pass
